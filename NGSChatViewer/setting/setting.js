8a327e48ff5c25f2f3b5facd299b024ad795f197
masayoshi4649 <masayoshi4649@gmail.com>
2021-08-06 05:18:29 +0900

    ChatLog 分離

---
+++ NGSChatViewer/setting/setting.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+
+function gridSetting() {
+    let returnJSON = {};
+    const defaultJSON = JSON.parse(
+        fs.readFileSync(__dirname + "\\default\\grid.json", "utf8")
+    );
+    const userJSON = JSON.parse(
+        fs.readFileSync(__dirname + "\\user\\grid.json", "utf8")
+    );
+
+    let keys = Object.keys(defaultJSON);
+    for (let i = 0; i < keys.length; i++) {
+        // ユーザ定義優先 null時default
+        returnJSON[keys[i]] = userJSON[keys[i]] || defaultJSON[keys[i]];
+    }
+    return returnJSON;
+}
+
+function actionLogSetting() {
+    let returnJSON = {};
+    const defaultJSON = JSON.parse(
+        fs.readFileSync(__dirname + "\\default\\actionLog.json", "utf8")
+    );
+    const userJSON = JSON.parse(
+        fs.readFileSync(__dirname + "\\user\\actionLog.json", "utf8")
+    );
+
+    let keys = Object.keys(defaultJSON);
+    for (let i = 0; i < keys.length; i++) {
+        // ユーザ定義優先 null時default
+        returnJSON[keys[i]] = userJSON[keys[i]] || defaultJSON[keys[i]];
+    }
+    return returnJSON;
+}
+
+function chatLogSetting() {
+    let returnJSON = {};
+    const defaultJSON = JSON.parse(
+        fs.readFileSync(__dirname + "\\default\\chatLog.json", "utf8")
+    );
+    const userJSON = JSON.parse(
+        fs.readFileSync(__dirname + "\\user\\chatLog.json", "utf8")
+    );
+
+    let keys = Object.keys(defaultJSON);
+    for (let i = 0; i < keys.length; i++) {
+        // ユーザ定義優先 null時default
+        returnJSON[keys[i]] = userJSON[keys[i]] || defaultJSON[keys[i]];
+    }
+    return returnJSON;
+}
+
+
+function writeActionLogSetting(jsondata) {
+    fs.writeFileSync(__dirname + "\\user\\actionLog.json", JSON.stringify(jsondata, null, "\t"));
+}
+
+function writeChatLogSetting(jsondata) {
+    fs.writeFileSync(__dirname + "\\user\\chatLog.json", JSON.stringify(jsondata, null, "\t"));
+}
+
+module.exports = {
+    gridSetting,
+    actionLogSetting,
+    chatLogSetting,
+    writeActionLogSetting,
+    writeChatLogSetting
+}
